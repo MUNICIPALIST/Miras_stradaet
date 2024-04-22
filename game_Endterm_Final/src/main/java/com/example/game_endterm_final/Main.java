25b52c2db7be6193b88a9550a49529f95871f39e
valtzman <xmodgamecenterhon@gmail.com>
2024-04-22 20:13:48 +0500

    next

--- game_Endterm_Final/src/main/java/com/example/game_endterm_final/Main.java
+++ game_Endterm_Final/src/main/java/com/example/game_endterm_final/Main.java
@@ -1,6 +1,7 @@
 package com.example.game_endterm_final;
 
 import java.net.URL;
+import java.nio.charset.CharsetEncoder;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -35,11 +36,32 @@ public class Main {
 
         btn.setText(String.valueOf(nowSym));
 
-        if (gameField[0][0] == gameField[0][1] && gameField[0][0] == gameField[0][2]) {
+        if (gameField[0][0] == gameField[0][1] && gameField[0][0] == gameField[0][2] && (gameField[0][0] == 'x' || gameField[0][0] == '0'))  {
             isGame = false;
-
-
         }
+        else if (gameField[1][0] == gameField[1][1] && gameField[1][0] == gameField[1][2] && (gameField[1][0] == 'x' || gameField[1][0] == '0'))  {
+            isGame = false;
+        }
+        else if (gameField[2][0] == gameField[2][1] && gameField[2][0] == gameField[1][2] && (gameField[2][0] == 'x' || gameField[2][0] == '0'))  {
+            isGame = false;
+        }
+        else if (gameField[0][0] == gameField[1][0] && gameField[0][0] == gameField[2][0] && (gameField[0][0] == 'x' || gameField[0][0] == '0'))  {
+            isGame = false;
+        }
+        else if (gameField[0][1] == gameField[1][1] && gameField[0][1] == gameField[2][1] && (gameField[0][1] == 'x' || gameField[0][1] == '0'))  {
+            isGame = false;
+        }
+        else if (gameField[0][2] == gameField[1][2] && gameField[0][2] == gameField[2][2] && (gameField[0][2] == 'x' || gameField[0][2] == '0'))  {
+            isGame = false;
+        }
+        else if (gameField[0][0] == gameField[1][1] && gameField[0][0] == gameField[2][2] && (gameField[0][0] == 'x' || gameField[0][0] == '0'))  {
+            isGame = false;
+        }
+        else if (gameField[2][2] == gameField[1][1] && gameField[2][2] == gameField[0][2] && (gameField[2][2] == 'x' || gameField[2][2] == '0'))  {
+            isGame = false;
+        }
+
+
 
         nowSym = nowSym == 'x' ? '0' : 'x';
 
