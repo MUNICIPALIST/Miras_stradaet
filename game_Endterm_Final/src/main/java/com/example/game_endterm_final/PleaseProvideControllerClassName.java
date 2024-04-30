35b6769ace1b432116f366888b255fabb1aa3523
Miras0007 <167431435+Miras0007@users.noreply.github.com>
2024-04-30 22:42:50 +0600

    from login Miras

---
+++ game_Endterm_Final/src/main/java/com/example/game_endterm_final/PleaseProvideControllerClassName.java
@@ -0,0 +1,99 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private Button changePass_backBtn;
+
+    @FXML
+    private PasswordField changePass_cPassword;
+
+    @FXML
+    private AnchorPane changePass_form;
+
+    @FXML
+    private PasswordField changePass_password;
+
+    @FXML
+    private Button changePass_proceedBtn;
+
+    @FXML
+    private TextField forgot_answer;
+
+    @FXML
+    private Button forgot_backBtn;
+
+    @FXML
+    private AnchorPane forgot_form;
+
+    @FXML
+    private Button forgot_proceedBtn;
+
+    @FXML
+    private ComboBox<?> forgot_selectQuestion;
+
+    @FXML
+    private TextField forgot_username;
+
+    @FXML
+    private Button login_btn;
+
+    @FXML
+    private Button login_createAccount;
+
+    @FXML
+    private Hyperlink login_forgotPassword;
+
+    @FXML
+    private AnchorPane login_form;
+
+    @FXML
+    private PasswordField login_password;
+
+    @FXML
+    private CheckBox login_selectShowPassword;
+
+    @FXML
+    private TextField login_showPassword;
+
+    @FXML
+    private TextField login_username;
+
+    @FXML
+    private AnchorPane main_form;
+
+    @FXML
+    private TextField signup_answer;
+
+    @FXML
+    private Button signup_btn;
+
+    @FXML
+    private PasswordField signup_cPassword;
+
+    @FXML
+    private TextField signup_email;
+
+    @FXML
+    private AnchorPane signup_form;
+
+    @FXML
+    private Button signup_loginAccount;
+
+    @FXML
+    private PasswordField signup_password;
+
+    @FXML
+    private ComboBox<?> signup_selectQuestion;
+
+    @FXML
+    private TextField signup_username;
+
+}
